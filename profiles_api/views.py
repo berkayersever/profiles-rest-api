e999afab032e5ce71006159685828fa665ce73ac
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-12 23:39:10 +0300

    Update views.py

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -19,3 +19,13 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'api_view': api_view})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
