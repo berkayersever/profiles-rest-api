208daa6d474292239e17ae2f16e52838243709ba
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-12 23:57:52 +0300

    Update views.py

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import viewsets, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from profiles_api import serializers
@@ -41,3 +41,18 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    def list(self, request):
+        """Returns a hello message"""
+
+        view_set = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URL\'s using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello!', 'view_set': view_set})
