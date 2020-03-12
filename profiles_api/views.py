142c2272724e70219f0b4b6b3eabfc79e747841b
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-13 01:33:13 +0300

    Update views.py

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -25,7 +25,7 @@ class HelloApiView(APIView):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
-            message = f'Hello {name}'
+            message = f'Hello {name}!'
             return Response({'message': message})
         else:
             return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
@@ -45,6 +45,7 @@ class HelloApiView(APIView):
 
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
 
     def list(self, request):
         """Returns a hello message"""
@@ -56,3 +57,13 @@ class HelloViewSet(viewsets.ViewSet):
         ]
 
         return Response({'message': 'Hello!', 'view_set': view_set})
+
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
