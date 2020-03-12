296fdc6ee2b27e0f9178a59158e2a5816578e6d8
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-12 22:30:57 +0300

    Update views.py

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,2 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
+        api_view = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to URL\'s',
+        ]
+
+        return Response({'message': 'Hello!', 'api_view': api_view})
