64c8de60a0656dc75f711bbc336b116b79f9b4b5
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-13 03:06:09 +0300

    Imported filters from rest_framework package

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status
+from rest_framework import filters, status, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
