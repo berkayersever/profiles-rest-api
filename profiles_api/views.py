e84c2530d4065f6b3cbab9bc8001bf6302d63b36
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-14 19:27:08 +0300

    Imported the IsAuthenticatedOrReadOnly package.

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,10 @@
 from rest_framework import filters, status, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
+from rest_framework.views import APIView
 from profiles_api import models, permissions, serializers
 
 
